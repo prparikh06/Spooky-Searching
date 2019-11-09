5753dcf97812dabf5c990a0556bb33435ca8baaf
Mazaya Rahman <mr1411@cpp.cs.rutgers.edu>
2019-11-09 16:51:45 -0500

    generate random list, set up macro

---
+++ multitest.h
@@ -0,0 +1,11 @@
+#ifndef _multitest_h_
+#define _multitest_h_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define search( arr, x ) divideUpWork( arr, x )
+
+int divideUpWork(int* arr, int target);
+
+#endif
---
+++ searchtest.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "multitest.h"
+
+int* shuffle(int* arr, int i){
+    size_t num = sizeof(arr)/sizeof(arr[0]);
+    int randInd = rand() % num;
+    int temp = arr[randInd];
+    arr[randInd] = arr[i];
+    arr[i] = temp;
+
+
+    return arr;
+
+}
+
+int main(int argc, char* argv[]){
+
+    int num = 20;
+    //FIRST GENERATION OF RANDOM LIST
+    int* array = malloc(num * sizeof(int));
+    int i = 0;
+    for(i = 0; i < num; i++){
+        array[i] = i;
+    }
+    for(i = 0; i < num; i++){
+        printf("%d ", array[i]);
+    }
+
+    for(i = 0; i < num; i++){
+        int randInd = rand() % num;
+        int temp = array[randInd];
+        array[randInd] = array[i];
+        array[i] = temp;
+    }
+    printf("\n");
+    for(i = 0; i < num; i++){
+        printf("%d ", array[i]);
+    }
+
+    //SEARCH USING PROCESSES/THREADS
+
+    ////////////////////////////
+
+    //EVERY TEST AFTERWARDS DOESN'T NEED TO GENERATE THE WHOLE LIST FROM SCRATCH, JUST SHUFFLE
+    int found = 5;
+    array = shuffle(array, found);
+    printf("\n");
+    for(i = 0; i < num; i++){
+        printf("%d ", array[i]);
+    }
+
+    //OTHER TESTS
+
+
+
+    return(0);
+
+
+
+}
