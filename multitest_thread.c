3c7c04c19001a806d8fef20d35b1c01482fcabdc
Priya Parikh <pp649@assembly.cs.rutgers.edu>
2019-11-14 22:54:10 -0500

    yoooo

--- multitest_thread.c
+++ multitest_thread.c
@@ -2,19 +2,35 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <math.h>
-#include <libpthread.h> 
+#include <pthread.h> 
+
+
+
 
 void* testingThreads(void* arg){
 
-    printf("my name is priya!!!!\n");
+	//do stuff here
+	printf("thead's ID: %d\n", (int*) arg);
 
+   
+//	pthread_exit(NULL);
 }
 //test creating and closing a thread
 int main(){
-    pthread_t num;
-    int status = pthread_create(&num,NULL,testingThreads, NULL);
-    if (status != 0) return 0; 
-    printf("creating thread...num is %d\n",num);
-    pthread_exit(num);
-    printf("thread exited!\n");
+   
+	int i = 0;
+	
+	pthread_t num1, num2;
+	int status1 = pthread_create(&num1,NULL,testingThreads, (void*) num1);
+    	int status2 = pthread_create(&num2,NULL,testingThreads, (void*) num2);
+    	if (status1 != 0 || status2 != 0) printf("error"); 
+	
+	pthread_join(num1, NULL);
+	pthread_join(num2, NULL);
+	
+
+
+pthread_exit(NULL);
+printf("thread has been exited\n");
+
 }
