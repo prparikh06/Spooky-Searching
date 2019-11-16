79aca32a7a8f96274249109fd52024cd9656a598
Mazaya Rahman <mr1411@cpp.cs.rutgers.edu>
2019-11-15 22:49:26 -0500

    thread works yayy, needs cleanup

--- multitest_thread.c
+++ multitest_thread.c
@@ -10,24 +10,36 @@ struct thread_arg{
     int start;
     int end;
     int ind;
+    int target;
 };
 
 //Function to search
 void* ThreadSearch(void* arg){
-    struct thread_arg* ptr = (struct thread_arg*)arg;
-    int start = ptr->start;
-    int end = ptr->end;
+    printf("target: %d\n", target);
+    //struct thread_arg* ptr = (struct thread_arg*)arg;
+    int* ptr = (int*)arg;
+    int start = ptr[0];
+    int end = ptr[1];
+    printf("start: %d, end: %d\n", start, end);
+    int f = 0;
     int i = 0;
     for(i = start; i<end; i++){
         if(mainArray[i] == target){
-            pthread_exit(i);
+            ptr[2] = i;
+            printf("true\n");
+            f = 1;
         }
     }
-    pthread_exit(251);
+    //if(f == 0){
+     //   ptr->ind = 251;
+    //}
+    printf("ind is: %d\n", ptr[2]);
+    pthread_exit(&arg);
 }
 int divideUpWork(int* arr, int targ, int num){
     mainArray = arr;
     target = targ;
+    printf("target: %d\n", target);
     int piece = 0; // # of nums in each section
     int numThreads = 0; // num of threads based on piece
     int threadCount = 0; // keeps count of number of threads
@@ -41,28 +53,44 @@ int divideUpWork(int* arr, int targ, int num){
     }
     printf("size of array: %d\n", num);
     //printf("piece: %d, numProcs: %d\n", piece, numProcs);
+    printf("numThreads: %d threadCount: %d\n", numThreads, threadCount);
 
     pthread_t thread[numThreads];
+    int k = 0;
     int i = 0;
-    for(i = 0; i < numThreads; i++){
-        struct thread_arg arg;
-        arg.start = i;
-        arg.end = i+piece;
-        pthread_create(&thread[i], NULL, ThreadSearch, (void*)&arg);
+    for(i = 0; i < num; i+= piece, k++){
+        pthread_t handler;
+
+        struct thread_arg* arg = malloc(sizeof(struct thread_arg));
+        arg->start = i;
+        arg->end = i+piece;
+        arg->target = targ;
+        int* meta = (int*)malloc(sizeof(int)*3);
+        meta[0] = i; meta[1] = i + piece; meta[2] = -1;
+        printf("arg start at: %d\n", arg->start);
+        int status = pthread_create(&handler, NULL, ThreadSearch, (void*)meta);
+        if(status != 0){
+            printf("error occurred while threading\n");
+        }
+        thread[k] = handler;
+        printf("handler: %d\n", handler);
         threadCount++;
     }
-
+    printf("numThreads: %d threadCount: %d\n", numThreads, threadCount);
     int found = 251;
     for(i = 0; i < numThreads; i++){
-        void* arg = NULL;
-        pthread_join(thread[i], &arg);
-        struct thread_arg* ptr = (struct thread_arg*)arg;
-        if(ptr->ind != NULL){
+        struct thread_arg* ptr;
+        pthread_join(thread[i], (void*)&ptr);
+        printf("joined thread: %d\n", i);
+        //struct thread_arg* ptr = (struct thread_arg*)arg;
+        int x = ptr->ind;
+        printf("x is: %d\n", x);
+        if(ptr->ind != 251){
             found = ptr->ind;
             found = (i*piece) + found;
         }
     }
-
+    pthread_exit(NULL);
     return found;
 
 
